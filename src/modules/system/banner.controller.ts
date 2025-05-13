f9eed8e3b2b4e7786f9aa2848fef664088f20c11
Nguyen Ngoc Anh Tuan <Tomtepfa@gmail.com>
2025-05-13 22:16:37 +0700

    feat: implement banner management

--- src/modules/system/banner.controller.ts
+++ src/modules/system/banner.controller.ts
@@ -1,7 +1,70 @@
-import { Controller } from '@nestjs/common';
+import { RBAC } from '@base/decorators/auth.decorator';
+import { UserRoles } from '@modules/user/enums/roles.enum';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { CreateBannerDto } from './dto/create-banner.dto';
+import { UpdateBannerDto } from './dto/update-banner.dto';
 import { SystemService } from './system.service';
-
+@ApiTags('Banners')
 @Controller('banners')
 export class BannerController {
   constructor(private readonly systemService: SystemService) {}
+
+  @RBAC(UserRoles.ADMIN)
+  @ApiOperation({ summary: 'Lấy danh sách banner' })
+  @Get('get-banners')
+  async getAllBanners() {
+    return await this.systemService.getAllBanners();
+  }
+
+  @ApiOperation({ summary: 'Lấy danh sách banner đang sử dụng' })
+  @Get('get-active-banners')
+  async getActiveBanners() {
+    return await this.systemService.getActiveBanners();
+  }
+
+  @RBAC(UserRoles.ADMIN)
+  @ApiOperation({ summary: 'Lấy banner theo id' })
+  @Get('get-banner/:id')
+  async getBannerById(@Param('id') id: string) {
+    return await this.systemService.findBannerById(id);
+  }
+
+  @ApiOperation({ summary: 'Tạo banner mới' })
+  @Post('create-banner')
+  async createBanner(@Body() createBannerDto: CreateBannerDto) {
+    return await this.systemService.createBannerAsync(createBannerDto);
+  }
+
+  @RBAC(UserRoles.ADMIN)
+  @ApiOperation({ summary: 'Cập nhật banner' })
+  @Patch('update-banner/:id')
+  async updateBanner(
+    @Param('id') id: string,
+    @Body() updateBannerDto: UpdateBannerDto,
+  ) {
+    return await this.systemService.updateBannerAsync(id, updateBannerDto);
+  }
+
+  @RBAC(UserRoles.ADMIN)
+  @ApiOperation({ summary: 'Xóa banner' })
+  @Delete('delete-banner/:id')
+  async deleteBanner(@Param('id') id: string) {
+    return await this.systemService.deleteBannerAsync(id);
+  }
+
+  @RBAC(UserRoles.ADMIN)
+  @ApiOperation({ summary: 'Tắt/bật banner' })
+  @Patch('toggle-status/:id')
+  async toggleBannerStatus(@Param('id') id: string) {
+    return await this.systemService.toggleBannerStatus(id);
+  }
 }
--- src/modules/system/system.service.ts
+++ src/modules/system/system.service.ts
@@ -1,6 +1,10 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { plainToInstance } from 'class-transformer';
 import { Repository } from 'typeorm';
+import { BaseBannerDto } from './dto/base-banner.dto';
+import { CreateBannerDto } from './dto/create-banner.dto';
+import { UpdateBannerDto } from './dto/update-banner.dto';
 import { Banner } from './entities/banner.entity';
 import { Config } from './entities/config.entity';
 
@@ -13,4 +17,73 @@ export class SystemService {
     @InjectRepository(Banner)
     private bannerRepository: Repository<Banner>,
   ) {}
+
+  async createBannerAsync(createBannerDto: CreateBannerDto) {
+    const banner = this.bannerRepository.create(createBannerDto);
+    const createdBanner = await this.bannerRepository.save(banner);
+    return plainToInstance(BaseBannerDto, createdBanner, {
+      excludeExtraneousValues: true,
+    });
+  }
+
+  async getAllBanners() {
+    const rawBanners = await this.bannerRepository.find({
+      order: {
+        createdAt: 'DESC',
+      },
+    });
+    return rawBanners.map((banner) => {
+      return plainToInstance(BaseBannerDto, banner, {
+        excludeExtraneousValues: true,
+      });
+    });
+  }
+
+  async getActiveBanners() {
+    const rawBanners = await this.bannerRepository.find({
+      where: { isActive: true },
+      order: {
+        createdAt: 'ASC',
+      },
+    });
+    return rawBanners.map((banner) => {
+      return plainToInstance(BaseBannerDto, banner, {
+        excludeExtraneousValues: true,
+      });
+    });
+  }
+
+  async findBannerById(id: string) {
+    const banner = await this.bannerRepository.findOneOrFail({ where: { id } });
+    return plainToInstance(BaseBannerDto, banner, {
+      excludeExtraneousValues: true,
+    });
+  }
+
+  async updateBannerAsync(id: string, updateBannerDto: UpdateBannerDto) {
+    const banner = await this.bannerRepository.findOneOrFail({ where: { id } });
+    const updatedBanner = await this.bannerRepository.save({
+      ...banner,
+      ...updateBannerDto,
+    });
+    return plainToInstance(BaseBannerDto, updatedBanner, {
+      excludeExtraneousValues: true,
+    });
+  }
+  async deleteBannerAsync(id: string) {
+    const banner = await this.bannerRepository.findOneOrFail({ where: { id } });
+    await this.bannerRepository.softDelete(banner.id);
+    return {
+      message: 'Banner đã được xóa thành công',
+    };
+  }
+
+  async toggleBannerStatus(id: string) {
+    const banner = await this.bannerRepository.findOneOrFail({ where: { id } });
+    banner.isActive = !banner.isActive;
+    const updatedBanner = await this.bannerRepository.save(banner);
+    return plainToInstance(BaseBannerDto, updatedBanner, {
+      excludeExtraneousValues: true,
+    });
+  }
 }
