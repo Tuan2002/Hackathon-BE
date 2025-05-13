5b2bfc698902123408ec93921b6d5e202685ed4c
Nguyen Ngoc Anh Tuan <Tomtepfa@gmail.com>
2025-05-13 21:46:24 +0700

    feat: update module imports in app.module and author.module for consistency

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,11 @@
 import { ConfigAppModule } from '@base/modules/configs/config-app.module';
 import { DatabaseModule } from '@base/modules/database/database.module';
 import { AuthModule } from '@modules/auth/auth.module';
+import { AuthorModule } from '@modules/author/author.module';
 import { CategoryModule } from '@modules/category/category.module';
+import { PublisherModule } from '@modules/publisher/publisher.module';
 import { SharedModule } from '@modules/shared/shared.module';
+import { SystemModule } from '@modules/system/system.module';
 import { UserModule } from '@modules/user/user.module';
 import { Module } from '@nestjs/common';
 import { PassportModule } from '@nestjs/passport';
@@ -15,6 +18,9 @@ import { PassportModule } from '@nestjs/passport';
     AuthModule,
     UserModule,
     CategoryModule,
+    AuthorModule,
+    PublisherModule,
+    SystemModule,
     SharedModule,
   ],
   controllers: [],
--- src/modules/author/author.module.ts
+++ src/modules/author/author.module.ts
@@ -1,13 +1,13 @@
 import { SharedModule } from '@modules/shared/shared.module';
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Author } from './entities/author.entity';
-import { AuthorService } from './author.service';
 import { AuthorController } from './author.controller';
+import { AuthorService } from './author.service';
+import { Author } from './entities/author.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Author]), SharedModule],
   controllers: [AuthorController],
   providers: [AuthorService],
 })
-export class PublisherModule {}
+export class AuthorModule {}
