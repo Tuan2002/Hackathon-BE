625d67e2b9a2b1a02d5ca23383f7b0dd4f2beba2
Nguyen Ngoc Anh Tuan <Tomtepfa@gmail.com>
2025-05-15 00:56:08 +0700

    feat: add Expose decorators to categoryName, authorName, and publisherName in DTOs

--- src/modules/document/dto/base-document.dto.ts
+++ src/modules/document/dto/base-document.dto.ts
@@ -1,4 +1,5 @@
 import { ApiProperty, PickType } from '@nestjs/swagger';
+import { Expose } from 'class-transformer';
 import { Document } from '../entities/document.entity';
 export class BaseDocumentDto extends PickType(Document, [
   'id',
@@ -20,11 +21,14 @@ export class BaseDocumentDto extends PickType(Document, [
   'updatedAt',
 ]) {
   @ApiProperty()
+  @Expose()
   categoryName?: string;
 
   @ApiProperty()
+  @Expose()
   authorName?: string;
 
   @ApiProperty()
+  @Expose()
   publisherName?: string;
 }
--- src/modules/document/dto/public-document.dto.ts
+++ src/modules/document/dto/public-document.dto.ts
@@ -1,4 +1,5 @@
 import { ApiProperty, PickType } from '@nestjs/swagger';
+import { Expose } from 'class-transformer';
 import { Document } from '../entities/document.entity';
 export class PublicDocumentDto extends PickType(Document, [
   'id',
@@ -21,11 +22,14 @@ export class PublicDocumentDto extends PickType(Document, [
   'updatedAt',
 ]) {
   @ApiProperty()
+  @Expose()
   categoryName?: string;
 
   @ApiProperty()
+  @Expose()
   authorName?: string;
 
   @ApiProperty()
+  @Expose()
   publisherName?: string;
 }
