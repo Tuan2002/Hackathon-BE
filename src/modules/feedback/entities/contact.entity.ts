edcc0aa85ff38e3c2700f0009b5638cc2bcd4eb2
Nguyen Ngoc Anh Tuan <Tomtepfa@gmail.com>
2025-05-17 22:50:44 +0700

    feat: add validation for optional phone and message fields in Contact entity

--- src/modules/feedback/entities/contact.entity.ts
+++ src/modules/feedback/entities/contact.entity.ts
@@ -2,7 +2,7 @@ import { AbstractEntity } from '@base/entities/base.entity';
 import { Table } from '@constants';
 import { ApiProperty } from '@nestjs/swagger';
 import { Expose } from 'class-transformer';
-import { IsEmail, IsString } from 'class-validator';
+import { IsEmail, IsString, ValidateIf } from 'class-validator';
 import { Column, Entity } from 'typeorm';
 
 @Entity(Table.Contact)
@@ -21,11 +21,14 @@ export class Contact extends AbstractEntity {
 
   @ApiProperty({ nullable: true })
   @Expose()
+  @ValidateIf((o) => o.phone !== undefined)
   @IsString()
   @Column({ nullable: true })
   phone?: string;
 
   @ApiProperty({ nullable: true })
+  @ValidateIf((o) => o.message !== undefined)
+  @IsString()
   @Expose()
   @Column({ nullable: true })
   message?: string;
